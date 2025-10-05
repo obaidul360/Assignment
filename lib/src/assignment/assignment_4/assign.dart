05185de66ac97f5dd93140d23e42603e7b2d1023
Md. Obaidul Islam <obdcse360@gmail.com>
2025-10-06 02:41:12 +0600

    assignment-3 submit

--- lib/src/assignment/assignment_4/assign.dart
+++ lib/src/assignment/assignment_4/assign.dart
@@ -11,7 +11,275 @@ class _AssignmentFourState extends State<AssignmentFour> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("data"),),
+      appBar: AppBar(
+        backgroundColor: Color(0xFFB3AFAF),
+        centerTitle: true,
+        title: Text("Contacts", style: TextStyle(color: Colors.white)),
+        leading: Icon(Icons.menu, size: 22, color: Colors.white),
+        actions: [
+          Icon(Icons.search, size: 30, color: Colors.white),
+          SizedBox(width: 7),
+          CircleAvatar(
+            radius: 18,
+            backgroundImage: AssetImage("assets/obaidul.jpg"),
+          ),
+          SizedBox(width: 7),
+        ],
+      ),
+      backgroundColor: Color(0xFFDCD5D3),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          spacing: 10,
+          children: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                     CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('assets/code.jpg'),
+                    ),
+
+                    // Green Dot (Online Status)
+                    Positioned(
+                      bottom: 2,
+                      right: 2,
+                      child: Container(
+                        height: 12,
+                        width: 12,
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white, width: 2),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(width: 10),
+
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Md. Mamun Islam mim",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "He is CoderAngoan Woner",
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    const CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('assets/obaidul.jpg'),
+                    ),
+
+                    // Green Dot (Online Status)
+                    Positioned(
+                      bottom: 2,
+                      right: 2,
+                      child: Container(
+                        height: 12,
+                        width: 12,
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white, width: 2),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(width: 10),
+
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Md. Obaidul Islam",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "Learning with Flutter",
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    const CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('assets/coder.jpg'),
+                    ),
+
+                    // Green Dot (Online Status)
+                    Positioned(
+                      bottom: 2,
+                      right: 2,
+                      child: Container(
+                        height: 12,
+                        width: 12,
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white, width: 2),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(width: 10),
+
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "ColderAngan",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "CoderAngoan Logo",
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    const CircleAvatar(
+                      radius: 30,
+                      backgroundImage: NetworkImage(
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtF1Gz_Xsh2r_DfO5JaLspe4oKYcEGo-myBg&s",
+                      ),
+                    ),
+
+                    // Green Dot (Online Status)
+                    Positioned(
+                      bottom: 2,
+                      right: 2,
+                      child: Container(
+                        height: 12,
+                        width: 12,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white, width: 2),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(width: 10),
+
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Nature",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "I love Nature",
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    const CircleAvatar(
+                      radius: 30,
+                      backgroundImage: NetworkImage(
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWqWOy0nQLal_IwCJriYw56LWwNr0jNJwfVg&s",
+                      ),
+                    ),
+
+                    // Green Dot (Online Status)
+                    Positioned(
+                      bottom: 2,
+                      right: 2,
+                      child: Container(
+                        height: 12,
+                        width: 12,
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white, width: 2),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(width: 10),
+
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Flutter",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "My Drem",
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
