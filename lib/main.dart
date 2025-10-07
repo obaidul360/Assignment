a1c7b89ab5190d023f0084dbee86f13e229ed75b
Md. Obaidul Islam <obdcse360@gmail.com>
2025-10-08 03:04:34 +0600

    Assignment-4 container, singlechildscrollview, stack,position,column, row etc. learn

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:coderangoan_assignment/src/assignment/assignment_3/assignment.dart';
 import 'package:coderangoan_assignment/src/assignment/assignment_4/assign.dart';
+import 'package:coderangoan_assignment/src/assignment/assignment_5.dart';
 
 import 'package:flutter/material.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
     return SafeArea(
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: AssignmentFour(),
+        home: AssignmentFive(),
       ),
     );
   }
